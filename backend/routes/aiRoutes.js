784d1e1618ea4969bf14e5e1e554755ae466ad44
Sree Madhav <sreemadhavmadaka@gmail.com>
2026-06-17 17:07:55 +0530

    Delete backend directory

--- backend/routes/aiRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { evaluateStartup } = require("../controllers/aiController");
-
-// The API endpoint: POST /api/ai/evaluate
-router.post("/evaluate", evaluateStartup);
-
-module.exports = router;
--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/authController");
-
-// The actual API endpoints
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-
-module.exports = router;
